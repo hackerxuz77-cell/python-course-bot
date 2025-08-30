1fcc83b6f15e2abe5db5822ea0ac74e1b5cb65de
hackerxuz77-cell <hackerxuz77@gmail.com>
2025-08-29 22:19:32 -0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,20 @@
+# Bot tokeni - @BotFather dan oling
+BOT_TOKEN = "8424469053:AAGNkE0VVI2Yr79SHfKaHvKIVS0gZM0idkY"
+
+# Guruh chat ID si
+GROUP_CHAT_ID = -1002998658965
+
+# Adminlar ro'yxati (o'z IDlaringizni qo'shing)
+ADMINS = [6965233988]
+
+# Ma'lumotlar bazasi nomi
+DATABASE_NAME = "bot.db"
+
+# Obuna muddati (oylarda)
+SUBSCRIPTION_MONTHS = 3
+
+# Jarima miqdori (so'mda)
+PENALTY_AMOUNT = 2000
+
+# Oylik to'lov miqdori (so'mda)
+MONTHLY_PAYMENT = 50000
